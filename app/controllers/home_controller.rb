00cdbc78d294af22a1a167a1990b4472fa41c402
Luiz <luizfelipebasile@gmail.com>
2017-12-06 19:23:28 -0200

    Resolvido problema de lances

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,34 +1,35 @@
 class HomeController < ApplicationController
-	helper_method :current_user
+  helper_method :current_user
 
 def current_user=(user)
   @current_user = user
 end
   def index
-  	@auctions = Auction.all
-  	@products = Product.all
-  	
+    @auctions = Auction.all
+    @products = Product.all
+    
   end
   def edit
-  	if !user_signed_in?
-	    flash[:error] = "Por Favor entre em sua conta para continuar"
+    if !user_signed_in? || current_user.email == "admin@gmail.com"
+      flash[:error] = "Por Favor entre em sua conta para continuar"
       redirect_to root_path
-	else   
+  else 
+   @auction = Auction.find(params[:id])
+     ap = auction_params
+     ap[:bet] = current_user.email  
     if @auction.update_attributes(ap)
-    	 @auction = Auction.find(params[:id])
-	    ap = auction_params
-	    ap[:bet] = current_user.email
+      
        flash[:success] = "Lance Atualizado, Boa sorte"
       redirect_to root_path
     else
        flash[:success] = "Erro"
       redirect_to root_path
     end
-end	 
+end  
   end
  private
   def auction_params
     params.permit(:bet)
   end
 end  
- 
+  
